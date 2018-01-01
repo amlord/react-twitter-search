b621a88d48f6d3fd3eda934dd6b8bac1d075d117
Andrew <email@andrewlord.co.uk>
2018-01-01 00:10:28 +0000

    added API URL check

--- src/api/Twitter.test.js
+++ src/api/Twitter.test.js
@@ -1,4 +1,4 @@
-import { CONSUMER_KEY, CONSUMER_SECRET } from './Twitter.conf';
+import { CONSUMER_KEY, CONSUMER_SECRET, API_URL } from './Twitter.conf';
 import { Twitter } from './Twitter';
 
 describe('Twitter API', () => {
@@ -11,14 +11,27 @@ describe('Twitter API', () => {
             expect( CONSUMER_SECRET ).toBeTruthy();
         });
 
+        it('API URL Set', () => {
+            // valid URL Regular Expression
+            const urlRegEx = /https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+
+            expect( urlRegEx.test( API_URL ) ).toBeTruthy();
+        });
+
         it('Encode Secret', () => {
             expect( Twitter.encodeSecret( CONSUMER_KEY, CONSUMER_SECRET ) ).toBeTruthy();
         });
     });
 
     describe('Authentication', () => {
-        it('Consumer Secret Set', () => {
-            //expect(CONSUMER_SECRET).toBeTruthy();
+        it('Get Bearer Token', () => {
+            
+        });
+    });
+
+    describe('Search', () => {
+        it('', () => {
+            
         });
     });
 });
